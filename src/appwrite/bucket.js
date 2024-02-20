863aafc35b6ff5bb8658e33c8e98fd3a5896d6c0
Kamlesh-Chowdary <kamleshchoyal143@gmail.com>
2024-02-19 20:48:57 -0800

    bucket(storage) service

---
+++ src/appwrite/bucket.js
@@ -0,0 +1,56 @@
+import conf from "../conf";
+import { Client, Storage, ID } from "appwrite";
+
+export class StorageService {
+  client = new Client();
+  bucket;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.bucket = new Storage(this.client);
+  }
+
+  async uploadFile(file) {
+    try {
+      return await this.bucket.createFile(
+        conf.appwriteBucketId,
+        ID.unique(),
+        file
+      );
+    } catch (error) {
+      console.log("Appwrite :: createFile :: error", error);
+    }
+  }
+
+  // async getFile(fileId) {
+  //   try {
+  //     return await this.bucket.getFile(conf.appwriteBucketId, fileId);
+  //   } catch (error) {
+  //     console.log("Appwrite :: getFile :: error", error);
+  //   }
+  // }
+
+  async deleteFile(fileId) {
+    try {
+      await this.bucket.deleteFile(conf.appwriteBucketId, fileId);
+      return true;
+    } catch (error) {
+      console.log("Appwrite :: deleteFile :: error", error);
+      return false;
+    }
+  }
+
+  getFilePreview(fileId) {
+    try {
+      this.bucket.getFilePreview(conf.appwriteBucketId, fileId);
+    } catch (error) {
+      console.log("Appwrite :: getFilePreview :: error", error);
+    }
+  }
+}
+
+const storageService = new StorageService();
+
+export default storageService;
