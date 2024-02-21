3294102e3ce08f9d318e05b14ed622b161669a36
Kamlesh-Chowdary <kamleshchoyal143@gmail.com>
2024-02-20 21:54:51 -0800

    BrowserRouter wrap

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store/store.js";
-
+import { BrowserRouter } from "react-router-dom";
+const router = {};
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter router={router}>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
